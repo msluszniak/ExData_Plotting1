a58d780366edd75f03ef016d2b29dcfe0d42672a
Mateusz <56299341+msluszniak@users.noreply.github.com>
2020-04-17 19:47:30 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,12 +1,19 @@
 # read data 2007-02-01 and 2007-02-02 only
 dataset = read.table(file = "household_power_consumption.txt", skip = 66637, nrows = 69517-66638+1, sep = ";")
+#set columns names
 colnames(dataset) = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 
+#plot 3
+#open Graphic device
 png(file = "plot3.png")
+#dplyr package 
+#add column with date format: dd/mm/YY HH:MM:SS
 dataset = mutate(dataset, dateNtime = as.POSIXct(paste(dataset$Date, dataset$Time), format="%d/%m/%Y %H:%M:%S"))
 with(dataset, plot(dateNtime, Sub_metering_1,ylab = "Energy sub metering",xlab = "", type = "n"))
 with(dataset, lines(dateNtime,Sub_metering_1, col = c("black")))
 with(dataset, lines(dateNtime,Sub_metering_2, col = c("red")))
 with(dataset, lines(dateNtime,Sub_metering_3, col = c("blue")))
+#add legend in topright corner with proper parameters
 legend("topright",lty = 1, col = c("black", "red" , "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3" ))
+#close Graphic device
 dev.off()
