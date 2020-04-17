9eb8fbea75ec22d7be8f7ec40cd8141693ac711f
Mateusz <56299341+msluszniak@users.noreply.github.com>
2020-04-17 19:25:33 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-#reading data
+#reading data 2007-02-01 and 2007-02-02
 dataset = read.table(file = "/home/mateusz/Desktop/Working_directory_R/household_power_consumption.txt", skip = 66637, nrows = 69517-66638+1, sep = ";")
 colnames(dataset) = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 
